daa3a95fe43f17f8e8a4849f5863ba7f04a11c76
pauline feytel <pauline.feytel@appliedtechnology.se>
2019-06-27 11:47:50 +0200

    Add select option for the country in Settings.js

--- client/src/News/News.js
+++ client/src/News/News.js
@@ -3,11 +3,12 @@ import './News.css';
 import OneNews from '../OneNews/OneNews';
 
 function News() {
-
+  
   const [news, setNews] = useState([]);
+  const NEWS_API_KEY = process.env.REACT_APP_NEWS_API_KEY;
 
   useEffect(() => {
-    fetch(`https://newsapi.org/v2/top-headlines?country=se&apiKey=***REMOVED***`)
+    fetch(`https://newsapi.org/v2/top-headlines?country=se&apiKey=${NEWS_API_KEY}`)
       .then(res => res.json())
       .then(data => setNews(data.articles))
   }, [])
--- client/src/Settings/Settings.js
+++ client/src/Settings/Settings.js
@@ -1,10 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Settings.css';
 
+const countries = [{ id: 'se', name: 'Sweden' }, { id: 'fr', name: 'France' }, { id: 'de', name: 'Germany' }, { id: 'gb', name: 'United Kingdom' }, { id: 'es', name: 'Spain' }, { id: 'it', name: 'Italy' }]
+
+
 function Settings() {
+  const [selectedCountry, setSelectedCountry] = useState('se');
+
   return (
     <div className="Settings">
-      Hello Settings
+      <select name="country">
+        {countries.map(country => (
+          <option value={country.id} selected={selectedCountry===country.id}>{country.name}</option>
+        ))}
+      </select>
     </div>
   );
 }
